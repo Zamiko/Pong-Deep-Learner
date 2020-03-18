0bb6a3cdd2f3d8dfa915a4482ff42686c915081e
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-18 23:33:51 +0000

    almost doen with act

--- dqn.py
+++ dqn.py
@@ -53,13 +53,16 @@ class QLearner(nn.Module):
             # TODO: Given state, you should write code to get the Q value and chosen action
             qvalues = self.forward(state.data)
             np_qvalues = qvalues.detach().cpu().numpy()
-            action = np_qvalues[np.argmax(np_qvalues)]
+            #action = 1
+            action = np_qvalues[0][np.argmax(np_qvalues)]
             print(qvalues.data[0])
-            print("Max val: ", action)
+            print(np_qvalues)
+            print(action)
             print("\n")
 
         else:
             action = random.randrange(self.env.action_space.n)
+
         return action
 
     def copy_from(self, target):
